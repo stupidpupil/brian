773266a357080f8fd90a700a0ff0630bd338dd1c
Adam Watkins <adam@stupidpupil.co.uk>
2012-09-22 17:46:26 +0100

    Hash-storage support

---
+++ lib/brain/hash.rb
@@ -0,0 +1,77 @@
+module Brain
+	class NeuralNetwork
+
+		def to_hash
+			layers = []
+			@sizes.count.times do |layer|
+				layers[layer] = {}
+
+				if layer == 0 and @input_lookup
+					nodes = @input_lookup.keys
+				elsif (layer == @output_layer) and @output_lookup
+					nodes = @output_lookup.keys
+				else
+					nodes = (0..@sizes[layer]-1).to_a
+				end
+
+
+				nodes.each_with_index do |node,j|
+					layers[layer][node] = {}
+
+					next if layer == 0
+					layers[layer][node][:bias] = @biases[layer][j]
+
+					layers[layer][node][:weights] = {}
+
+					layers[layer-1].keys.each do |k|
+						index = k
+						index = @input_lookup[k] if (layer == 1) and @input_lookup
+						
+						layers[layer][node][:weights][k] = @weights[layer][j][index]	
+					end
+				end
+			end
+
+			return {layers:layers}
+		end
+
+		def self.new_with_hash(hash)
+			net = NeuralNetwork.new
+
+			net.instance_eval do
+				size = hash[:layers].count
+				@output_layer = size -1
+
+				@sizes = Array.new(size)
+				@weights = Array.new(size)
+				@biases = Array.new(size)
+				@outputs = Array.new(size)
+
+				hash[:layers].each_with_index do |layer, i|
+					if i == 0 and layer[0].nil?
+						@input_lookup = Brain::Lookup.lookup_from_hash(layer)
+					end
+
+					if i == @output_layer and layer[0].nil?
+						@output_lookup = Brain::Lookup.lookup_from_hash(layer)
+					end
+
+					nodes = layer.keys
+
+					@sizes[i] = nodes.count
+					@weights[i] = []
+					@biases[i] = []
+					@outputs[i] = []
+
+					nodes.each_with_index do |node, j|
+						@biases[i][j] = layer[node][:bias]
+						@weights[i][j] = layer[node][:weights].nil? ? nil : layer[node][:weights].values
+					end
+				end
+
+			end
+			return net
+		end
+
+	end
+end
---
+++ spec/hash_spec.rb
@@ -0,0 +1,41 @@
+require 'brain'
+require 'brain/hash'
+
+describe Brain::NeuralNetwork do
+	describe "Can store and retrieve XOR-network" do
+		before do
+			@net = Brain::NeuralNetwork.new
+			@net.train([
+				{input: [0, 0], output: [0]},
+				{input: [0, 1], output: [1]},
+				{input: [1, 0], output: [1]},
+				{input: [1, 1], output: [0]}])
+		end
+
+		it "Produces identical output" do 
+			net2 = Brain::NeuralNetwork.new_with_hash(@net.to_hash)
+
+			input = [1,0]
+			@net.run(input).should eql net2.run(input)
+		end
+	end
+
+	describe "Can store and retrieve the Colour Contrast-network" do
+		before do
+			@net = Brain::NeuralNetwork.new
+			@net.train([
+				{input: { r: 0.03, g: 0.7, b: 0.5 }, output: { black: 1.0 }},
+	           	{input: { r: 0.16, g: 0.09, b: 0.2 }, output: { white: 1.0 }},
+    	       	{input: { r: 0.5, g: 0.5, b: 1.0 }, output: { white: 1.0 }}])
+		end
+
+
+		it "Produces identical output" do 
+			net2 = Brain::NeuralNetwork.new_with_hash(@net.to_hash)
+
+			input = { r: 1, g: 0.4, b: 0 }
+			@net.run(input).should eql net2.run(input)
+		end
+
+	end
+end
